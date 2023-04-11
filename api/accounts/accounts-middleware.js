6c6386c0f54c1031d03e5657ff4ef4ff78be6afa
Kristian Rosales <r.kristian40@gmail.com>
2023-04-11 12:00:47 -0500

    i hungy

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,32 @@
-exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
+const Account = require("./accounts-model");
+
+exports.checkAccountPayload = async (req, res, next) => {
+  try {
+    const { name, budget } = req.body;
+    if (!name || !budget) {
+      res.status(400).json({ message: "name and budget are required" });
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
+  console.log("checkAccountNameUnique fired");
+  next();
 };
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id);
+    if (!account) {
+      res.status(404).json({ message: "not found" });
+    } else {
+      req.account = account;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 };
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,42 +1,60 @@
 const router = require("express").Router();
+const middleware = require("./accounts-middleware");
+const Account = require("./accounts-model");
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    res.json([{}, {}]);
+    const accounts = await Account.getAll();
+    res.json(accounts);
   } catch (err) {
     next({ status: 422, message: "this is horrible" });
   }
 });
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", middleware.checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    res.json("get accounts by id");
+    const accountId = await Account.getById(req.params.id);
+    if (!accountId) {
+      res.status(404).json({ message: "account not found" });
+    } else {
+      res.json(accountId);
+    }
   } catch (err) {
     next(err);
   }
 });
 
-router.post("/", (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-    res.json("post accounts");
-  } catch (err) {
-    next(err);
+router.post(
+  "/",
+  middleware.checkAccountPayload,
+  middleware.checkAccountNameUnique,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json("post accounts");
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
-router.put("/:id", (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-    res.json("update account");
-  } catch (err) {
-    next(err);
+router.put(
+  "/:id",
+  middleware.checkAccountId,
+  middleware.checkAccountPayload,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      res.json("update account");
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", middleware.checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
   try {
     res.json("delete account");
