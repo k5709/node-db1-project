dd9916a9daf0e8b5e78638d2ccdd42e08a0ae4b2
Kristian Rosales <r.kristian40@gmail.com>
2023-04-11 13:22:42 -0500

    halfway

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,28 @@
 const Account = require("./accounts-model");
 
 exports.checkAccountPayload = async (req, res, next) => {
-  try {
-    const { name, budget } = req.body;
-    if (!name || !budget) {
-      res.status(400).json({ message: "name and budget are required" });
+  const { name, budget } = req.body;
+
+  if (!name || !budget) {
+    res.status(400).json({ message: "name and budget are required" });
+  } else {
+    const trimmedName = name.trim();
+    if (trimmedName.length < 3 || trimmedName.length > 100) {
+      res
+        .status(400)
+        .json({ message: "name of account must be between 3 and 100" });
+    } else {
+      const parsedBudget = Number(budget);
+      if (Number.isNaN(parsedBudget)) {
+        res.status(400).json({ message: "budget must be a number" });
+      } else if (parsedBudget < 0 || parsedBudget > 1000000) {
+        res
+          .status(400)
+          .json({ message: "budget must be between 0 and 1000000" });
+      } else {
+        next();
+      }
     }
-  } catch (err) {
-    next(err);
   }
 };
 
@@ -21,7 +36,7 @@ exports.checkAccountId = async (req, res, next) => {
   try {
     const account = await Account.getById(req.params.id);
     if (!account) {
-      res.status(404).json({ message: "not found" });
+      res.status(404).json({ message: "account not found" });
     } else {
       req.account = account;
       next();
